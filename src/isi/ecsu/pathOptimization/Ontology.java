3cfcf1bfd7cfecadb89310c0d89a1a73706d343f
Subhasis Dasgupta <dasgupta.subhasis@gmail.com>
2014-06-03 13:46:08 +0530

    updated PO set impl

---
+++ src/isi/ecsu/pathOptimization/Ontology.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package isi.ecsu.pathOptimization;
+
+import com.hp.hpl.jena.ontology.OntModel;
+
+/**
+ * @author subhasis
+ *
+ */
+public class Ontology {
+	private OntModel model;
+	
+	public OntModel readOntologyVirtuoso(OntModel lmodel){
+		
+		
+		
+		
+		
+		return model;
+	}
+	
+
+}
