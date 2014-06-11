b83f76171a91b3f977feccb4aa9a147c6ccb1e24
Subhasis Dasgupta <dasgupta.subhasis@gmail.com>
2014-06-11 22:54:32 +0530

    view model

--- src/isi/ecsu/view/struct/impl/viewGenerationModule.java
+++ src/isi/ecsu/view/struct/impl/viewGenerationModule.java
@@ -3,11 +3,36 @@
  */
 package isi.ecsu.view.struct.impl;
 
+import com.hp.hpl.jena.rdf.model.Model;
+
+import isi.ecsu.view.struct.OntologyConcept;
+
 /**
  * @author subhasis
  *
  */
-public class viewGenerationModule {
+public class viewGenerationModule implements View {
+	
+	/**
+	 * @param user
+	 */
+	public viewGenerationModule(String user) {
+		this.user = user;
+	}
+	private String user;
+	private String roleUser;
+	private OntologyConcept oc;
+	private String ontoDatabase;
+	
+	public Model viewModel(String user, String role){
+		
+		
+		
+		return null;
+		
+		
+	}
+	
 	
 	
 
