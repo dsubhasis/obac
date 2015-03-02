31891254945dfbc03b3000056e0bea4c2049e280
Subhasis Dasgupta <dasgupta.subhasis@gmail.com>
2015-03-02 15:58:58 +0530

    Object Traversal

---
+++ src/isi/ecsu/security/TraverseOntology.java
@@ -0,0 +1,124 @@
+/**
+ * 
+ */
+package isi.ecsu.security;
+
+import isi.ecsu.Util.CommonConstant;
+import isi.ecsu.Util.commonUtil;
+import isi.ecsu.view.struct.ViewObject;
+import isi.ecsu.view.struct.impl.StorageAccess;
+import isi.ecsu.view.struct.impl.VirtDataAccess;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+
+/**
+ * @author subhasis
+ *
+ */
+
+
+public class TraverseOntology {
+	
+	private Logger slf4jLogger = LoggerFactory
+			.getLogger(TraverseOntology.class);
+	private ViewObject vo;
+	public void intiateAccess(String rootElement, String graphName, String parentURI) throws Throwable{
+		
+		slf4jLogger.info("View Details "+rootElement+"Graph Name"+graphName+parentURI);
+		this.getUserView(rootElement, graphName, parentURI);
+		
+		
+		
+	}
+	
+	
+	
+	public ViewObject getUserView( String rootElement, String graphName, String parentURI)
+			throws Throwable {
+		Map<String, List<String>> roleNodeSet = new HashMap<String, List<String>>();
+		String lparentURI = parentURI;
+		List<String> unVisited = new LinkedList<String>();
+		int unvisitedCount = 0;
+		RoleAccess raccess = new RoleAccess();
+		StorageAccess virt = new VirtDataAccess();
+		String prefix = CommonConstant.prefix01;
+		vo.getNodeElement().put(lparentURI, 1);
+		vo.getPositiveList().add(lparentURI);
+		do {
+			// System.out.println("\n Executing Query On  : " +lparentURI+
+			// " Concept" );
+			String query = commonUtil.queryListSubClassNode(graphName,
+					lparentURI, CommonConstant.relation00, prefix);
+			//System.out.println("\nQuery \t" + query);
+			slf4jLogger.info("\nQuery \t" + query);
+			ResultSet subClasses = virt.executeQuery(query);
+			while (subClasses.hasNext()) {
+				QuerySolution row = subClasses.next();
+				RDFNode x = row.get("cls");
+				int permRoot = raccess.getPermission(x.toString(), "roleName");
+				query = commonUtil.queryListParentClassNode(graphName,
+						x.toString(), CommonConstant.relation00, prefix);
+				ResultSet parentList = virt.executeQuery(query);
+				while (parentList.hasNext()) {
+					QuerySolution parentRow = parentList.next();
+					RDFNode p = parentRow.get("cls");
+				}
+				if (parentList.getRowNumber() > 1) {
+					vo.getMultipleParent().add(x.toString());
+				}
+				System.out.println("\n For the Concept  " + x.toString()
+						+ "  Number of Paernt Found "
+						+ parentList.getRowNumber());
+				int permChild = raccess.getPermission(x.toString(), "roleName");
+
+				if (permRoot == 1 || permChild == 1) {
+					vo.getNodeElement().put(x.toString(), 1);
+					vo.getPositiveList().add(x.toString());
+				}
+				if (permRoot == 0 || permChild == 0) {
+
+					vo.getNodeElement().put(x.toString(), 0);
+					vo.getNegativeList().add(x.toString());
+				}
+				if (!unVisited.contains(x.toString())) {
+					unVisited.add(x.toString());
+				} else {
+					if (vo.getMultipleParent().contains(x.toString())) {
+						unVisited.add(x.toString());
+					}
+				}
+			}
+			unvisitedCount = unVisited.size();
+			if (unvisitedCount > 0) {
+				lparentURI = unVisited.get(unVisited.size() - 1);
+				unVisited.remove(lparentURI);
+			}
+		} while (unvisitedCount > 0);
+		System.out.println("\n Analysis Done Generating View for the Role ");
+		return vo;
+	}
+
+
+
+	/**
+	 * 
+	 */
+	public TraverseOntology() {
+		
+		slf4jLogger.info("Object Created");
+	}
+	
+	
+	
+
+}
