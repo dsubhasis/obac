7d302a3ff5109fc6d69004ad94c22e333a84366d
Subhasis Dasgupta <dasgupta.subhasis@gmail.com>
2015-03-02 17:58:37 +0530

    Data Structure for Ontology access

---
+++ src/isi/ecsu/view/struct/OntologyObject.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package isi.ecsu.view.struct;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author subhasis
+ *
+ */
+public class OntologyObject {
+	
+	 private List<String> NodeList = new LinkedList<String>();
+	    
+	    private List<String> multipleParent = new LinkedList<String>();
+	   
+	    /**
+	     * 
+	     */
+	    public OntologyObject() {
+	    }
+	    /**
+	     * @return the positiveList
+	     */
+	   
+	    public final List<String> getMultipleParent() {
+	        return multipleParent;
+	    }
+	    /**
+		 * @return the nodeList
+		 */
+		public final List<String> getNodeList() {
+			return NodeList;
+		}
+		/**
+		 * @param nodeList the nodeList to set
+		 */
+		public final void setNodeList(List<String> nodeList) {
+			NodeList = nodeList;
+		}
+		/**
+	     * @param multipleParent the multipleParent to set
+	     */
+	    public final void setMultipleParent(List<String> multipleParent) {
+	        this.multipleParent = multipleParent;
+	    }
+	    /**
+	     * @return the nodeElement
+	     */
+	    
+	    
+	    
+
+}
