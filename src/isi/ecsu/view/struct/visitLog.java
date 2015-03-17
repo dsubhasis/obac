b6f28335ce61643104099cd932533e25c3f02b4c
Subhasis Dasgupta <dasgupta.subhasis@gmail.com>
2015-03-17 17:51:14 +0530

    New Data structure

---
+++ src/isi/ecsu/view/struct/visitLog.java
@@ -0,0 +1,50 @@
+package isi.ecsu.view.struct;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class visitLog {
+	
+	
+	List UnVisited;
+	List ParentName;
+	Map ParentPermissionMap;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public visitLog() {
+		
+		UnVisited = new ArrayList();
+		ParentName = new ArrayList();
+		
+	}
+	
+	public Map addPermission(String parentNode, int perM)
+	{
+		return ParentPermissionMap = new HashMap();
+		
+		
+		
+	}
+	
+	
+
+}
