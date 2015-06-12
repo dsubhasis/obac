c1b4fc4e26d940aa461f9284b557b3fb6f3110c8
Subhasis Dasgupta <dasgupta.subhasis@gmail.com>
2015-06-12 22:56:27 +0530

    JSON Commit

--- src/isi/ecsu/view/struct/impl/ReConstructionView.java
+++ src/isi/ecsu/view/struct/impl/ReConstructionView.java
@@ -3,71 +3,94 @@
  */
 package isi.ecsu.view.struct.impl;
 
-import java.util.ArrayList;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.server.UID;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.json.simple.JSONObject;
+
 import isi.ecsu.view.struct.visitLog;
 
-import com.hp.hpl.jena.ontology.OntClass;
-import com.hp.hpl.jena.ontology.OntModel;
-import com.hp.hpl.jena.ontology.OntModelSpec;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.Property;
 
 /**
  * @author subhasis
  *
  */
 public class ReConstructionView {
+	private JSONObject dlo;
 	
 	
-	public void rebuiltOntology(visitLog vl){
+	/**
+	 * @return the JSON Object of view 
+	 */
+	public final JSONObject getJSONView() {
+		return dlo;
+	}
+
+
+	public void rebuiltOntology(visitLog vl) throws MalformedURLException{
 		
-		OntModel m = ModelFactory.createOntologyModel( OntModelSpec.OWL_DL_MEM);
-		String uri = "http://www.iwi-iuk.org/material/RDF/Schema/Class/scf#Property-III";
-		m.createProperty("http://ontology.project/#hasContributedto");
-		Property hCt = m.getProperty("http://ontology.project/#hasContributedto");
-		Map<String, Map> vs = vl.getVisitNodeList();
-		OntClass randClass = null;
-		for (Map.Entry<String, Map> entry : vs.entrySet())
-		{
-		    List<String> lParent =  new ArrayList<String>();
-		    List<String> lSuper = new ArrayList<String>();
-			System.out.println(entry.getKey() + "/" + entry.getValue());
-		    Map<String , Integer>  parentClass = entry.getValue();
-		    randClass = m.createClass(entry.getKey()); 
-		    for ( Map.Entry<String, Integer> pEntry : parentClass.entrySet())
-		    {
-		    	if(pEntry.getValue() == 0)
-		    	{
-		    	lParent.add(pEntry.getKey());
-		    	if(vl.getVisitNodeList().containsKey(pEntry.getKey()))
-		    			{
-		    		       lSuper.add(pEntry.getKey());
-		    			}
-		    	}
-		    }
-		    
-		    randClass = m.createClass(entry.getKey()); 
-		    Iterator<String> lPar = null;
-		    Iterator<String> lsup = null;
-		    
-		    lsup = lSuper.iterator();
-		    while(lsup.hasNext())
-		    {
-		    	randClass.addSuperClass(lsup.next());
-		    }
-		    
-		    randClass.addSuperClass();
-		    
+		dlo = new JSONObject();
+
+		UID viewId = new UID();
+		
+		dlo.put("Version", 1.0);
+		dlo.put("id", "dlview"+viewId.toString() );
+		System.out.println();
+		
+		Map<String, Map> listNode = vl.getVisitNodeList(); 
+		
+		for(Map.Entry<String, Map> entry : listNode.entrySet()){
+			System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+			String key = entry.getKey();
+			Map parentList = listNode.get(key);
 		}
+		Map<String, Integer> userRoot = listNode.get("userRootNode");
+		String rootNode = null;
+		for(Map.Entry<String, Integer> entry : userRoot.entrySet()){
+			rootNode = entry.getKey();
+		}
+		listNode.remove("userRootNode");
+		Map jsonRoot = new HashMap();
+		jsonRoot.put("rootNode", new URL(rootNode));
+		for(Map.Entry<String, Map> entryObj : listNode.entrySet()){
+		Map<String, Integer> parentList = new HashMap();
+		parentList = entryObj.getValue();
+		URL concept = new URL(entryObj.getKey());
+	    List parentStruct = new LinkedList();
+	    Map parentMap = new HashMap();
+	   // parentMap.put("Concept", concept);
+		for(Map.Entry<String, Integer> entryConcept : parentList.entrySet())
+		{
+			List parentConcept = new LinkedList();
+			String parentNode = entryConcept.getKey();
+		   Integer permV = entryConcept.getValue();
+		   parentConcept.add(new URL(parentNode));
+		   parentConcept.add(permV);
+		   parentStruct.add(parentConcept);
+		}
+		parentMap.put("parent", parentStruct);
+		jsonRoot.put(concept, parentMap);
+		
 		
 		
 		
 		
 	}
-
+		
+	
+		
+	
+	dlo.putAll(jsonRoot);
+	
+	System.out.println();
+	
+}
 }
