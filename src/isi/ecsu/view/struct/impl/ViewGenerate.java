8989f8f6d51aa7c7e3ddca9ea852e42326403c6c
Subhasis Dasgupta <dasgupta.subhasis@gmail.com>
2015-06-12 23:21:59 +0530

    JSON Commit

---
+++ src/isi/ecsu/view/struct/impl/ViewGenerate.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package isi.ecsu.view.struct.impl;
+
+import isi.ecsu.Util.CommonConstant;
+
+import com.hp.hpl.jena.ontology.OntModel;
+
+/**
+ * @author subhasis
+ *
+ */
+public class ViewGenerate {
+	public void executeView(String rootObject, String userGroupName)
+	{
+		
+		
+
+		View vw = new ViewGenerationModule();
+		String domainName = "www.semanticweb.org.subhasis.ontologies.2014.6.untitled-ontology-15";
+		//String roleName = CommonConstant.userGraphURL, rootObject = "DL-Concept19";
+		OntModel lmodel = null;
+		try {
+			System.out.println("Version 1.0");
+			OntModel om = vw.roleView(lmodel, userGroupName, rootObject);
+			System.out.println("Version 1.0");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+		
+	}
+
+}
